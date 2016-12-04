6a8b62bd29a4fabb4e0792be2bf62240deac0087
0xcc-Since2016 <0xccsince2016@gmail.com>
2016-12-05 02:54:52 +0800

    Change Theme and Icon For test

--- app/src/main/java/com/example/administrator/droideye/TrafficMonitor/TrafficInsActivity.java
+++ app/src/main/java/com/example/administrator/droideye/TrafficMonitor/TrafficInsActivity.java
@@ -3,6 +3,7 @@ package com.example.administrator.droideye.TrafficMonitor;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
 
 import com.example.administrator.droideye.R;
 
